3d2dbd042d156b811bbc95124b98775f84500e7d
Natnael kebede <71388535+natikebede@users.noreply.github.com>
2022-03-28 17:08:46 +0300

    update

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
+import { Button,Input,Container,Col,Row,FormGroup,Label,Form} from 'reactstrap';
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container bg-dark text-light rounded">
+      <div className="row">
+        <div className='col'>
+          <Form className="mt-3">
+            <FormGroup row>
+            <Label md={2} className="text-dark">phone number</Label>
+            <Col md={10}>
+          <Input type='text' name="number" className='w-50'/>
+          </Col>
+          </FormGroup>
+          <FormGroup row>
+            <Label md={2} className="text-dark">Message</Label>
+            <Col md={10}>
+          <Input type='textarea'rows='5' name="text area" className='w-50'/>
+          </Col>
+          <Col> 
+          <Button className='btn btn-primary' color='primary'> send</Button>
+          </Col>
+          </FormGroup>
+
+          </Form>
+
+        </div>
+
+      </div>
+
+      
     </div>
   );
 }
